c027922541389ba8c0c87341a3cb362773abef46
Sriram <peetlasreeamulu40@gmil.com>
2026-05-13 06:30:03 +0530

    model added

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "commerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "blog",
+        "USER": "root",
+        "PASSWORD": "9000840354@",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
